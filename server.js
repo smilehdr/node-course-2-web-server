b112411ca0e0dd40943e0f218830b954bf54d269
Hendra <smilehdr@gmail.com>
2019-02-01 10:25:28 +0700

    Add Project page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    // res.send('About Page');
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.get('/bad',(req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
